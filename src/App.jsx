fa7578bca8fba7dcf2cc60d82ec7d7e3bc8994e0
Calm Andy <thought7878@qq.com>
2024-01-19 19:18:42 +0800

    feat(components): add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { ConnectWallet } from '@thirdweb-dev/react';
+import { Route, Routes } from 'react-router-dom';
+import { Navbar, Sidebar } from './components';
+import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
 
-export default function Home() {
+export default function App() {
   return (
     <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
-      <div className='sm:flex hidden mr-10 relative'>Sidebar</div>
+      <div className='sm:flex hidden mr-10 relative'>
+        <Sidebar />
+      </div>
 
       <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
-        Navbar
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
       </div>
     </div>
   );
