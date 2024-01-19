a71f81bab2461b8c7e8c17689cbe371c9502b3fb
Calm Andy <thought7878@qq.com>
2024-01-19 17:21:04 +0800

    feat(pages): add architecture for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import { ConnectWallet } from '@thirdweb-dev/react';
 
 export default function Home() {
-  return <main className='main'>App</main>;
+  return (
+    <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
+      <div className='sm:flex hidden mr-10 relative'>Sidebar</div>
+
+      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+        Navbar
+      </div>
+    </div>
+  );
 }
