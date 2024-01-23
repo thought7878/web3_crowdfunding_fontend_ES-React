bbd90a2b82fa6e20c70ad207b88fa9f08b15d7d9
Calm Andy <thought7878@qq.com>
2024-01-23 13:53:04 +0800

    feat(Loader): add loader

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -2,7 +2,6 @@ import React, { useContext, createContext } from 'react';
 import {
   useAddress,
   useContract,
-  useMetamask,
   useConnect,
   useContractWrite,
   metamaskWallet,
--- src/pages/CreateCampaign.jsx
+++ src/pages/CreateCampaign.jsx
@@ -4,6 +4,7 @@ import { ethers } from 'ethers';
 import { money } from '../../public/assets';
 import { Button, FormField } from '../components';
 import { checkIfImage } from '../utils';
+import Loader from '../components/Loader';
 import { useStateContext } from '../context';
 
 const CreateCampaign = () => {
@@ -43,6 +44,7 @@ const CreateCampaign = () => {
 
   return (
     <div className='bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4'>
+      {isLoading && <Loader />}
       <div className='flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]'>
         <h1 className='font-bold sm:text-[25px] text-[18px] leading-[38px]'>
           Start a Campaign
